9e099290b82c9374d01c9e0887ee12e4826b07b6
Kieran Emery <kizwiz@hotmail.co.uk>
2024-05-17 04:09:53 +0100

    Add project files.

---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,41 @@
+﻿using FactoryDesignPattern.Credit_Cards;
+
+namespace FactoryDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // hardcording the card type
+            string cardType = "Monzo";
+
+            ICreditCard cardDetails = null;
+
+            if (cardType == "MoneyBack")
+            {
+                cardDetails = new MoneyBack();
+            }
+            else if (cardType == "Monzo")
+            {
+                cardDetails = new Monzo();
+            }
+            else if (cardType == "Platinum")
+            {
+                cardDetails = new Platinum();
+            }
+
+            if (cardDetails != null)
+            {
+                Console.WriteLine("CardType: " + cardDetails.GetCardType());
+                Console.WriteLine("CreditLimit: " + cardDetails.GetCreditLimit());
+                Console.WriteLine("ANnualCharge: " + cardDetails.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
